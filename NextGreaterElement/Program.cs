11bee7260858f1c6297d19d96421d5c071bcb6e7
gibeulah <gibeulah@microsoft.com>
2018-09-05 23:17:26 +0530

    Add project files.

---
+++ NextGreaterElement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NextGreaterElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
