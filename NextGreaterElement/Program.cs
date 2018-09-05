7a301c1652daaf58304aaf3cf4fd1590c5456a73
gibeulah <gibeulah@microsoft.com>
2018-09-06 01:37:06 +0530

    First Commit

--- NextGreaterElement/Program.cs
+++ NextGreaterElement/Program.cs
@@ -6,7 +6,42 @@ namespace NextGreaterElement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Next Greater Element!");
+            Console.WriteLine("--------------------");
+            int[] inputArray;
+            Console.WriteLine("Enter the number of elements");
+            int noOfElements = int.Parse(Console.ReadLine());
+            inputArray = new int[noOfElements];
+            Stack stack = new Stack(noOfElements);
+            Console.WriteLine("Enter the " + noOfElements + " elements by hitting Enter");
+            for (int i = 0; i < noOfElements; i++) {
+                inputArray[i] = int.Parse(Console.ReadLine());
+            }
+            stack.Push(inputArray[0]);
+            PrintNextGreaterElement(stack, inputArray);
+        }
+
+        public static void PrintNextGreaterElement(Stack stack, int[] array) {
+            for (int i = 1; i < array.Length; i++) {
+                if (stack.IsEmpty()) {
+                    stack.Push(array[i]);
+                }
+
+                while (stack.IsEmpty() == false && stack.Peek() < array[i]) {
+                    Console.WriteLine("Element: "+stack.Peek() +", Next Greater Element: "+array[i]);
+                    stack.Pop();
+                }
+                stack.Push(array[i]);
+            }
+            PrintMinusOneForThoseNoNGE(stack);
+        }
+
+        public static void PrintMinusOneForThoseNoNGE(Stack stack) {
+            while (stack.IsEmpty() == false) {
+                Console.WriteLine("Element: "+stack.Peek()+", NGE: -1");
+                stack.Pop();
+            }
+            Console.ReadKey();
         }
     }
 }
---
+++ NextGreaterElement/Stack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NextGreaterElement
+{
+    class Stack
+    {
+        private int top = -1;
+        private int size = 0;
+        private int[] array;
+        public Stack(int size) {
+            this.size = size;
+            array = new int[size];
+        }
+
+        public void Push(int Element) {
+            if (top == size - 1)
+            {
+                Console.WriteLine("Stack is full!");
+            }
+            else
+            {
+                top = top + 1;
+                array[top] = Element;     
+            }
+        }
+
+        public int Peek() {
+            return array[top];
+        }
+
+        public bool IsEmpty() {
+            return top == -1;
+        }
+
+        public int Pop() { 
+            if(top == -1){
+                Console.WriteLine("Stack is Empty! Cannot pop :(");
+                return -1;
+            }
+            int index = top ;
+            top -= 1;
+            return array[index];
+        }
+
+        public int Size() {
+            return size;
+        }
+    }
+}
